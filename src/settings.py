976b3f8d887a3fc021035392121f4121712b8056
alex <voroncov.exe@gmail.com>
2024-04-27 20:46:17 +0300

    fix

--- src/settings.py
+++ src/settings.py
@@ -8,12 +8,11 @@ DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
 
 class Settings(BaseSettings):
-    db_url: str
     session_db_url: Optional[str] = None
     items_per_user: int = 50
 
     model_config = SettingsConfigDict(env_file=DOTENV)
-    db_url: str = "postgresql://postgres:postgres@db:5444/postgres"
+    db_url: str = "postgresql://postgres:postgres@db:5432/postgres"
     secret: str = "ea270b06-bf10-46fd-9d1a-088c559f1694"
     algorithm: str = "HS256"
 
