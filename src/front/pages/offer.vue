424d060e10a94560bfd2912192e1bedb58cd4365
nosanov.s <nosanov.sergey@gmail.com>
2024-04-28 11:29:31 +0300

    added some blocks

---
+++ src/front/pages/offer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="offer">
+       <Offer />
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        setup() {
+          return {
+          }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import "./styles/offer/offer";
+</style>
